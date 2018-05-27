b9844244000bbb1d39be8e685621fbee6396eba8
Michael Ieti <m.ieti001@gmail.com>
2018-05-27 13:13:01 +1200

    Add ScheduleType to ParaTaskTest

--- workStealingBenchmark/src/uoa/se751/group22/sample/ParaTaskTest.java
+++ workStealingBenchmark/src/uoa/se751/group22/sample/ParaTaskTest.java
@@ -1,113 +1,120 @@
 package uoa.se751.group22.sample;//####[1]####
 //####[1]####
 import java.util.ArrayList;//####[3]####
-import uoa.se751.group22.utils.Work;//####[5]####
-//####[5]####
-//-- ParaTask related imports//####[5]####
-import pt.runtime.*;//####[5]####
-import java.util.concurrent.ExecutionException;//####[5]####
-import java.util.concurrent.locks.*;//####[5]####
-import java.lang.reflect.*;//####[5]####
-import pt.runtime.GuiThread;//####[5]####
-import java.util.concurrent.BlockingQueue;//####[5]####
-import java.util.ArrayList;//####[5]####
-import java.util.List;//####[5]####
-//####[5]####
-public class ParaTaskTest {//####[7]####
-    static{ParaTask.init();}//####[7]####
-    /*  ParaTask helper method to access private/protected slots *///####[7]####
-    public void __pt__accessPrivateSlot(Method m, Object instance, TaskID arg, Object interResult ) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {//####[7]####
-        if (m.getParameterTypes().length == 0)//####[7]####
-            m.invoke(instance);//####[7]####
-        else if ((m.getParameterTypes().length == 1))//####[7]####
-            m.invoke(instance, arg);//####[7]####
-        else //####[7]####
-            m.invoke(instance, arg, interResult);//####[7]####
-    }//####[7]####
-//####[9]####
-    public static void main(String[] args) {//####[9]####
-        ArrayList<Integer> itemsToProcess = new ArrayList<Integer>();//####[10]####
-        int numItems = 20;//####[12]####
-        int granularityOfTask = 100;//####[13]####
-        TaskIDGroup group = new TaskIDGroup(numItems);//####[15]####
-        for (int i = 0; i < numItems; i++) //####[17]####
-        {//####[17]####
-            itemsToProcess.add(granularityOfTask);//####[18]####
-            TaskID id = oneOffDoingWork(granularityOfTask);//####[19]####
-            group.add(id);//####[20]####
-        }//####[21]####
-        System.out.println("Will process " + numItems + " items with granularity of " + granularityOfTask);//####[23]####
-        long startTime = System.currentTimeMillis();//####[25]####
-        try {//####[27]####
-            group.waitTillFinished();//####[28]####
-        } catch (Exception e) {//####[29]####
-            e.printStackTrace();//####[30]####
-        }//####[31]####
-        long totalTime = System.currentTimeMillis() - startTime;//####[33]####
-        System.out.println("Time: " + totalTime + " milliseconds");//####[34]####
-    }//####[35]####
-//####[37]####
-    private static volatile Method __pt__oneOffDoingWork_int_method = null;//####[37]####
-    private synchronized static void __pt__oneOffDoingWork_int_ensureMethodVarSet() {//####[37]####
-        if (__pt__oneOffDoingWork_int_method == null) {//####[37]####
-            try {//####[37]####
-                __pt__oneOffDoingWork_int_method = ParaTaskHelper.getDeclaredMethod(new ParaTaskHelper.ClassGetter().getCurrentClass(), "__pt__oneOffDoingWork", new Class[] {//####[37]####
-                    int.class//####[37]####
-                });//####[37]####
-            } catch (Exception e) {//####[37]####
-                e.printStackTrace();//####[37]####
-            }//####[37]####
-        }//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(int granularity) {//####[37]####
-        //-- execute asynchronously by enqueuing onto the taskpool//####[37]####
-        return oneOffDoingWork(granularity, new TaskInfo());//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(int granularity, TaskInfo taskinfo) {//####[37]####
-        // ensure Method variable is set//####[37]####
-        if (__pt__oneOffDoingWork_int_method == null) {//####[37]####
-            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[37]####
-        }//####[37]####
-        taskinfo.setParameters(granularity);//####[37]####
-        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[37]####
-        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(TaskID<Integer> granularity) {//####[37]####
-        //-- execute asynchronously by enqueuing onto the taskpool//####[37]####
-        return oneOffDoingWork(granularity, new TaskInfo());//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(TaskID<Integer> granularity, TaskInfo taskinfo) {//####[37]####
-        // ensure Method variable is set//####[37]####
-        if (__pt__oneOffDoingWork_int_method == null) {//####[37]####
-            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[37]####
-        }//####[37]####
-        taskinfo.setTaskIdArgIndexes(0);//####[37]####
-        taskinfo.addDependsOn(granularity);//####[37]####
-        taskinfo.setParameters(granularity);//####[37]####
-        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[37]####
-        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(BlockingQueue<Integer> granularity) {//####[37]####
-        //-- execute asynchronously by enqueuing onto the taskpool//####[37]####
-        return oneOffDoingWork(granularity, new TaskInfo());//####[37]####
-    }//####[37]####
-    private static TaskID<Void> oneOffDoingWork(BlockingQueue<Integer> granularity, TaskInfo taskinfo) {//####[37]####
-        // ensure Method variable is set//####[37]####
-        if (__pt__oneOffDoingWork_int_method == null) {//####[37]####
-            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[37]####
-        }//####[37]####
-        taskinfo.setQueueArgIndexes(0);//####[37]####
-        taskinfo.setIsPipeline(true);//####[37]####
-        taskinfo.setParameters(granularity);//####[37]####
-        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[37]####
-        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[37]####
-    }//####[37]####
-    public static void __pt__oneOffDoingWork(int granularity) {//####[37]####
-        doWork(granularity);//####[38]####
-    }//####[39]####
-//####[39]####
-//####[41]####
-    private static void doWork(int granularity) {//####[41]####
-        Work.computeWork(granularity);//####[42]####
-    }//####[43]####
-}//####[43]####
+import pt.runtime.ParaTask;//####[5]####
+import pt.runtime.ParaTask.ScheduleType;//####[6]####
+import uoa.se751.group22.utils.Work;//####[7]####
+//####[7]####
+//-- ParaTask related imports//####[7]####
+import pt.runtime.*;//####[7]####
+import java.util.concurrent.ExecutionException;//####[7]####
+import java.util.concurrent.locks.*;//####[7]####
+import java.lang.reflect.*;//####[7]####
+import pt.runtime.GuiThread;//####[7]####
+import java.util.concurrent.BlockingQueue;//####[7]####
+import java.util.ArrayList;//####[7]####
+import java.util.List;//####[7]####
+//####[7]####
+public class ParaTaskTest {//####[9]####
+    static{ParaTask.init();}//####[9]####
+    /*  ParaTask helper method to access private/protected slots *///####[9]####
+    public void __pt__accessPrivateSlot(Method m, Object instance, TaskID arg, Object interResult ) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {//####[9]####
+        if (m.getParameterTypes().length == 0)//####[9]####
+            m.invoke(instance);//####[9]####
+        else if ((m.getParameterTypes().length == 1))//####[9]####
+            m.invoke(instance, arg);//####[9]####
+        else //####[9]####
+            m.invoke(instance, arg, interResult);//####[9]####
+    }//####[9]####
+//####[12]####
+    /** Sets schedule type for program *///####[12]####
+    private static final ScheduleType SCHEDULE_TYPE = ScheduleType.MixedSchedule;//####[12]####
+//####[14]####
+    public static void main(String[] args) {//####[14]####
+        ParaTask.setScheduling(SCHEDULE_TYPE);//####[15]####
+        ArrayList<Integer> itemsToProcess = new ArrayList<Integer>();//####[16]####
+        int numItems = 20;//####[18]####
+        int granularityOfTask = 100;//####[19]####
+        TaskIDGroup group = new TaskIDGroup(numItems);//####[21]####
+        for (int i = 0; i < numItems; i++) //####[23]####
+        {//####[23]####
+            itemsToProcess.add(granularityOfTask);//####[24]####
+            TaskID id = oneOffDoingWork(granularityOfTask);//####[25]####
+            group.add(id);//####[26]####
+        }//####[27]####
+        System.out.println("Will process " + numItems + " items with granularity of " + granularityOfTask);//####[29]####
+        System.out.println("Using ScheduleType: " + SCHEDULE_TYPE.toString());//####[30]####
+        long startTime = System.currentTimeMillis();//####[32]####
+        try {//####[34]####
+            group.waitTillFinished();//####[35]####
+        } catch (Exception e) {//####[36]####
+            e.printStackTrace();//####[37]####
+        }//####[38]####
+        long totalTime = System.currentTimeMillis() - startTime;//####[40]####
+        System.out.println("Time: " + totalTime + " milliseconds");//####[41]####
+    }//####[42]####
+//####[44]####
+    private static volatile Method __pt__oneOffDoingWork_int_method = null;//####[44]####
+    private synchronized static void __pt__oneOffDoingWork_int_ensureMethodVarSet() {//####[44]####
+        if (__pt__oneOffDoingWork_int_method == null) {//####[44]####
+            try {//####[44]####
+                __pt__oneOffDoingWork_int_method = ParaTaskHelper.getDeclaredMethod(new ParaTaskHelper.ClassGetter().getCurrentClass(), "__pt__oneOffDoingWork", new Class[] {//####[44]####
+                    int.class//####[44]####
+                });//####[44]####
+            } catch (Exception e) {//####[44]####
+                e.printStackTrace();//####[44]####
+            }//####[44]####
+        }//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(int granularity) {//####[44]####
+        //-- execute asynchronously by enqueuing onto the taskpool//####[44]####
+        return oneOffDoingWork(granularity, new TaskInfo());//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(int granularity, TaskInfo taskinfo) {//####[44]####
+        // ensure Method variable is set//####[44]####
+        if (__pt__oneOffDoingWork_int_method == null) {//####[44]####
+            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[44]####
+        }//####[44]####
+        taskinfo.setParameters(granularity);//####[44]####
+        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[44]####
+        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(TaskID<Integer> granularity) {//####[44]####
+        //-- execute asynchronously by enqueuing onto the taskpool//####[44]####
+        return oneOffDoingWork(granularity, new TaskInfo());//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(TaskID<Integer> granularity, TaskInfo taskinfo) {//####[44]####
+        // ensure Method variable is set//####[44]####
+        if (__pt__oneOffDoingWork_int_method == null) {//####[44]####
+            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[44]####
+        }//####[44]####
+        taskinfo.setTaskIdArgIndexes(0);//####[44]####
+        taskinfo.addDependsOn(granularity);//####[44]####
+        taskinfo.setParameters(granularity);//####[44]####
+        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[44]####
+        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(BlockingQueue<Integer> granularity) {//####[44]####
+        //-- execute asynchronously by enqueuing onto the taskpool//####[44]####
+        return oneOffDoingWork(granularity, new TaskInfo());//####[44]####
+    }//####[44]####
+    private static TaskID<Void> oneOffDoingWork(BlockingQueue<Integer> granularity, TaskInfo taskinfo) {//####[44]####
+        // ensure Method variable is set//####[44]####
+        if (__pt__oneOffDoingWork_int_method == null) {//####[44]####
+            __pt__oneOffDoingWork_int_ensureMethodVarSet();//####[44]####
+        }//####[44]####
+        taskinfo.setQueueArgIndexes(0);//####[44]####
+        taskinfo.setIsPipeline(true);//####[44]####
+        taskinfo.setParameters(granularity);//####[44]####
+        taskinfo.setMethod(__pt__oneOffDoingWork_int_method);//####[44]####
+        return TaskpoolFactory.getTaskpool().enqueue(taskinfo);//####[44]####
+    }//####[44]####
+    public static void __pt__oneOffDoingWork(int granularity) {//####[44]####
+        doWork(granularity);//####[45]####
+    }//####[46]####
+//####[46]####
+//####[48]####
+    private static void doWork(int granularity) {//####[48]####
+        Work.computeWork(granularity);//####[49]####
+    }//####[50]####
+}//####[50]####
